9dc6f48badb00e60110ca8fff2c9b6c510cea654
Samuel Adegboyega <gboyebolt@gmail.com>
2024-12-11 12:02:56 +0100

    Adding Animation Capabilites to the Script

--- Scripts/2d_Solver_steadyFlow.py
+++ Scripts/2d_Solver_steadyFlow.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as lin
 import argparse
+from matplotlib.animation import FuncAnimation
 
 class FDM_Solver_2D_SteadyFlow:
     def __init__(self, Nx, Ny, boundary_cond:dict):
@@ -36,10 +37,15 @@ class FDM_Solver_2D_SteadyFlow:
 
     def solveLinearSystem(self, A, b):
         t = lin.solve(A, b)
-        print(t.T)
         t= t.reshape((self.Nx - 1, self.Ny - 1))
         t=np.flip(t,axis=0)
-        return t
+        t2d=np.zeros((self.Nx+1,self.Ny+1))
+        t2d[0,:]=self.boundary_cond['top']
+        t2d[-1,:]=self.boundary_cond['buttom']
+        t2d[:,0]=self.boundary_cond['left']
+        t2d[:,-1]=self.boundary_cond['right']
+        t2d[1:-1,1:-1]=t
+        return t,t2d
     
     def plot(self,t):
         t2d=np.zeros((self.Nx+1,self.Ny+1))
@@ -60,6 +66,29 @@ class FDM_Solver_2D_SteadyFlow:
         plt.colorbar(im,ax=ax,label='Color Mapping')
        
         plt.show()
+        
+        return t2d
+    
+    def create_frame(self,step,ax):
+        for cond in list(self.boundary_cond.keys()):
+            self.boundary_cond[cond]=int(self.boundary_cond[cond])*np.cos(step)
+        # print(self.boundary_cond)
+        fdm = FDM_Solver_2D_SteadyFlow(Nx=self.Nx, Ny=self.Ny, boundary_cond=self.boundary_cond)
+        A = fdm.buildCoeffMatrix()
+        b = fdm.buildRHSVector()
+        t , t2d= fdm.solveLinearSystem(A, b)
+        ax.cla()
+        image=ax.imshow(t2d,cmap='jet')
+        
+        return image
+        
+    def animate(self):
+        fig = plt.figure()
+        ax = fig.gca()
+        animation = FuncAnimation(fig, self.create_frame, frames=10, fargs=(ax,))
+        animation.save('animation.mp4', writer='ffmpeg', fps=1)
+
+
 
 
 if __name__ == "__main__":
@@ -89,6 +118,7 @@ if __name__ == "__main__":
     parser.add_argument("--Ny",type=int)
     parser.add_argument('--Bc',action=ParseDict,metavar="KEY=VALUE",nargs="+")
     parser.add_argument('--plot',type=bool)
+    parser.add_argument('--animate',type=bool)
     
     args = parser.parse_args()
     Nx=args.Nx
@@ -98,12 +128,20 @@ if __name__ == "__main__":
         raise ValueError('You have to specify the boundary_condition')
     resources_per_worker={k:int(v) for k,v in zip(list(boundary_cond.keys()),list(boundary_cond.values()))}
     plot=args.plot
+    animate=args.animate
     
     fdm = FDM_Solver_2D_SteadyFlow(Nx=Nx, Ny=Ny, boundary_cond=boundary_cond)
     A = fdm.buildCoeffMatrix()
     b = fdm.buildRHSVector()
-    t = fdm.solveLinearSystem(A, b)
+    t ,t2d= fdm.solveLinearSystem(A, b)
     
+    if animate:
+        fdm.animate()
+    
+    del A,b
     if plot:
-        t_=fdm.plot(t)
-    print(t)
+        plot=fdm.plot(t)
+    
+    
+        
+        
